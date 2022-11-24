e74dc38909e097544e2c3550eeaf88e88e30da20
afdalabdallah <afdalabdallahm@gmai.com>
2022-11-24 23:07:18 +0700

    Add CRUD and Login

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebproQuiz2.Data;
+
+namespace WebproQuiz2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebproQuiz2.Data.Book> Book { get; set; }
+    }
+}
